5014df5aa44af01b9e4189d76ae589ffbcbdfba9
Sarthak Chauhan <sarthak.chauhan@123789.org>
2020-10-23 16:42:01 +0530

    gradient progress pub removed

--- lib/ui/widgets/hometabs/home_tab_view.dart
+++ lib/ui/widgets/hometabs/home_tab_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:gradient_progress/gradient_progress.dart';
+import 'package:lincus_maternity/controls/gradient-progress.dart';
 import 'package:lincus_maternity/models/app_constants.dart';
 import 'package:lincus_maternity/models/pregnancy/response/get_pregnancy_detail_response.dart';
 import 'package:lincus_maternity/models/wellbeing/response/get_wellbeing_score_response.dart';
